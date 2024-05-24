8a80239858f7d4d20031d1b191eebf24d2aa961f
L3earl <sainknight87@gmail.com>
2024-05-24 17:34:40 +0900

    1

--- day11/tmp.py
+++ day11/tmp.py
@@ -1,31 +1,94 @@
+# 차원 축소 예제: 유방암 데이터셋
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-np.random.seed(2020)
-data = np.random.randn(100)  # 평균 0, 표준편차 1의 분포에서 100개의 숫자를 샘플링한 데이터 생성
-data = np.concatenate((data, np.array([8, 10, -3, -5])))      # [8, 10, -3, -5])를 데이터 뒤에 추가함
-data
+# 데이터 로드
+cancer=load_breast_cancer()
 
-Q3, Q1 = np.percentile(data, [75 ,25])
-IQR = Q3 - Q1
-IQR
+# y=0(Malignant:악성 종양), y=1(Benign:양성 종양)
+cancer_X, cancer_y= cancer.data, cancer['target']
+train_X, test_X, train_y, test_y = train_test_split(cancer_X, cancer_y, test_size=0.1, random_state=10) # train 데이터셋과 test 데이터셋으로 나눔
+print(f"전체 검사자 수: {len(cancer_X)}")
+print(f"Train dataset에 사용되는 검사자 수: {len(train_X)}")
+print(f"Test dataset에 사용되는 검사자 수: {len(test_X)}")
+cancer_df = pd.DataFrame(cancer_X, columns=cancer['feature_names'])
+cancer_df.head()
 
-data[(Q1-1.5*IQR > data)|(Q3+1.5*IQR < data)]
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from collections import Counter
 
-# Q. 사분위 범위수를 이용해서 이상치를 찾는 outlier2() 함수를 구현해보세요.
-def outlier2(df, col):
-    # [[YOUR CODE]]
-    Q3, Q1 = np.percentile(df, [75 ,25])
-    IQR = Q3 - Q1
-    np.where(df[(Q1-1.5*IQR > df)|(Q3+1.5*IQR < df)])
+# color dictionary
+color_dict = {0: 'red', 1: 'blue', 2:'red', 3:'blue'}
+target_dict = {0: 'malignant_train', 1: 'benign_train', 2: 'malignant_test', 3:'benign_test'}
+
+#Train data에 PCA 알고리즘 적용
+train_X_ = StandardScaler().fit_transform(train_X) # 불러온 데이터에 대한 정규화 -> 각 column의 range of value가 전부 다르기 때문에 정규화를 진행해 주어야 합니다.
+train_df = pd.DataFrame(train_X_, columns=cancer['feature_names'])
+pca = PCA(n_components=2) # 주성분의 수를 2개, 즉 기저가 되는 방향벡터를 2개로 하는 PCA 알고리즘 수행
+pc = pca.fit_transform(train_df)
+
+#Test data에 PCA 알고리즘 적용
+test_X_ = StandardScaler().fit_transform(test_X) # normalization
+test_df = pd.DataFrame(test_X_, columns=cancer['feature_names'])
+pca_test = PCA(n_components=2)
+pc_test = pca_test.fit_transform(test_df)
+
+# 훈련한 classifier의 decision boundary를 그리는 함수
+def plot_decision_boundary(X, clf, ax): 
+    h = .02  # step size in the mesh
+    # create a mesh to plot in
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    ax.contour(xx, yy, Z, cmap='Blues')
+
+# PCA를 적용한 train data의 classifier 훈련: classfier로 Support Vector Machine(SVM) 사용
+clf = svm.SVC(kernel = 'rbf', gamma=0.5, C=0.8) # 여기서는 classifier로 SVM을 사용한다는 정도만 알아둡시다!
+clf.fit(pc, train_y) # train data로 classifier 훈련
+
+# PCA를 적용하지 않은 original data의 SVM 훈련
+clf_orig = svm.SVC(kernel = 'rbf', gamma=0.5, C=0.8) # 여기서는 classifier로 SVM을 사용한다는 정도만 알아둡시다!
+clf_orig.fit(train_df, train_y)
+
+# 캔버스 도식
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+# malignant와 benign의 SVM decision boundary 그리기
+plot_decision_boundary(pc, clf, ax)
+
+#Train data 도식
+for cluster in range(2):
+    sub_cancer_points = pc[train_y == cluster]
+    ax.scatter(sub_cancer_points[:, 0], sub_cancer_points[:, 1], edgecolor=color_dict[cluster], c='none', label=target_dict[cluster])
+# #Test data 도식
+# for cluster in range(2):
+#     sub_cancer_points = pc_test[test_y == cluster]
+#     ax.scatter(sub_cancer_points[:, 0], sub_cancer_points[:, 1], marker= 'x', c=color_dict[cluster+2], label=target_dict[cluster+2])
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_title('PCA-Breast cancer dataset')
+ax.legend()
+ax.grid()
+
+plt.savefig('PCA_Breast_cancer.png')
+
+# Scoring
+pca_test_accuracy_dict = Counter(clf.predict(pc_test) == test_y)
+orig_test_accuracy_dict = Counter(clf_orig.predict(test_df) == test_y)
+
+print("PCA 분석을 사용한 Test dataset accuracy: {}명/{}명 => {:.3f}".format(pca_test_accuracy_dict[True], sum(pca_test_accuracy_dict.values()), clf.score(pc_test, test_y)))
+print("PCA를 적용하지 않은 Test dataset accuracy: {}명/{}명 => {:.3f}".format(orig_test_accuracy_dict[True], sum(orig_test_accuracy_dict.values()), clf_orig.score(test_df, test_y)))
 
 
-outlier2(data)
 
-
-
-
-
-
-outlier2(trade, '무역수지')
 
 
